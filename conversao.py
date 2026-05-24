3b397633f961cd3c3e248e9b33290a5b8ce1f551
Gadev122 <gabrieelavelar@gmail.com>
2026-05-24 15:59:04 -0300

    Correções

--- conversao.py
+++
@@ -1,6 +0,0 @@
-import funcoes
-valor = input('Qual o valor?: ')
-base = input('Qual sua base?: ')
-nova_base = input('Qual sua nova base?: ')
-decimal = funcoes.basepradeci(valor, int(base))
-print (funcoes.deciprabase(decimal, int(nova_base)))
