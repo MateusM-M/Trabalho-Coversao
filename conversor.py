198400d394fe2fbfcd6987241dd1979f6666d6cb
MateusM-M <mateus.m.machado2007@gmail.com>
2026-05-25 14:56:16 -0300

    meu trabalho local com os csv

--- conversor.py
+++ conversor.py
@@ -1,6 +1,32 @@
-import funcoes
-valor = input('Qual o valor?: ')
-base = input('Qual sua base?: ')
-nova_base = input('Qual sua nova base?: ')
-decimal = funcoes.basepradeci(valor, int(base))
-print (funcoes.deciprabase(decimal, int(nova_base)))
+def deciprabase(numero, base):
+    termos = "0123456789ABCDEF"
+    if numero == 0:
+        return "0"
+    lista = []
+    while numero > 0:
+        resto = numero % base
+        numero = numero // base
+        lista.append(termos[resto])
+    lista.reverse()
+    return "".join(lista)
+
+def basepradec(numero, base):
+    termos = "0123456789ABCDEF"
+    cont = 0
+    decimal = 0
+    numero_str = str(numero)
+    num_d_algaris = len(numero_str)
+    for i in range(num_d_algaris - 1, -1, -1):
+            caractere = numero_str[i]
+            num_para_calc = termos.index(caractere)
+            decimal += num_para_calc * base ** cont
+            cont += 1
+    return decimal        
+
+def validar(valor, base):
+    termos = "0123456789ABCDEF"
+    termos_validos = termos[0:base]
+    for caractere in str(valor).upper():
+        if caractere not in termos_validos:
+            return False 
+        return True
