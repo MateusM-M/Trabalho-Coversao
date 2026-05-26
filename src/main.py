e39e584703f830053c8af7a36f00557a7a65fe02
GABRIEL DE AVELAR SILVA <gabriel.silva85@DCC07-20002060.ad.ufla.br>
2026-05-26 12:18:13 -0300

    Main atualizada

--- src/main.py
+++ src/main.py
@@ -1,17 +1,25 @@
 import conversor as fc
-modo = input("Escolha o modo da aplicação: 1(modo normal), 2(modo csv) 3(modo quiz)")
-if modo == "1":
-    valor = input('Qual o valor?: ')
-    base_origem = int(input('Qual sua base?: '))
-    base_saida = int(input('Qual sua nova base?: '))
-    if base_origem == 10 and (base_saida == 2 or base_saida == 8 or base_saida == 16):
-        print(fc.decimalparabase(valor, base_saida)) 
-    elif (base_origem == 2 or base_origem == 8 or base_origem == 16) and base_saida == 10:
-        print(fc.baseparadecimal(valor, base_origem))
-    elif (base_origem == 2 and base_saida == 8) or (base_origem == 8 and base_saida == 2):
-    
-    elif (base_origem == 16 and base_saida == 2) or (base_origem == 2 and base_saida == 16):
-    elif (base_origem == 8 and base_saida == 16) or (base_origem == 16 and base_saida == 8):
-elif modo == "2":
+import entrada as ent
 
-elif modo == "3":
+modo = int(input("Escolha o modo da aplicação: 1(modo normal), 2(modo csv) 3(modo quiz): ").strip())
+
+match modo:
+    case 1:
+        print("Modo normal selecionado.")
+        valor = ent.valor
+        base_origem = int(ent.base)
+        base_saida = int(ent.nova_base)
+        if base_origem == 10 and (base_saida == 2 or base_saida == 8 or base_saida == 16):
+            print(fc.decimalparabase(valor, base_saida)) 
+        elif (base_origem == 2 or base_origem == 8 or base_origem == 16) and base_saida == 10:
+            print(fc.baseparadecimal(valor, base_origem))
+        elif (base_origem == 2 and base_saida == 8) or (base_origem == 8 and base_saida == 2):
+            print(fc.bin_oct_hex(valor, base_origem, base_saida))
+        elif (base_origem == 16 and base_saida == 2) or (base_origem == 2 and base_saida == 16):
+            print(fc.bin_oct_hex(valor, base_origem, base_saida))
+        elif (base_origem == 8 and base_saida == 16) or (base_origem == 16 and base_saida == 8):
+            print(fc.oct_hex(valor, base_origem, base_saida))
+    case 2:
+        print("Modo csv selecionado.")
+        
+
