d50f90767c2b6d7f82680aa96c85446ecf3b1218
Matz_l <mateus.m.machado2007@gmail.com>
2026-05-26 22:05:49 -0300

    Refactor error message for clarity

--- src/saida.py
+++ src/saida.py
@@ -1,6 +1,5 @@
 def printa_valor(valorfinal):
     print(f"O valor na base pedida é {valorfinal}.")
 
-
 def mensagem_de_erro(valor, base):
-    print(f"Por favor o {valor} não pode estar na base {base}.")
+    print(f"O {valor} não pode estar na base {base}.")
