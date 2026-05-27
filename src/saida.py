5dcad6a4fea161440e557b9b3c70a73ff78c5223
Matz_l <mateus.m.machado2007@gmail.com>
2026-05-26 21:14:20 -0300

    Refactor error message formatting in saida.py

--- src/saida.py
+++ src/saida.py
@@ -3,12 +3,4 @@ def printa_valor(valorfinal):
 
 
 def mensagem_de_erro(valor, base):
-    print(f"Por favor coleguinha presta atenção {valor} não pode estar na base{base}.")
-
-
-def printa_valor(valorfinal):
-    print(f"O valor na base pedida é {valorfinal}.")
-
-
-def mensagem_de_erro(valor, base):
-    print(f"Por favor o {valor} não pode estar na base{base}.")
+    print(f"Por favor o {valor} não pode estar na base {base}.")
